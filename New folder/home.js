2fd66dc0ef1b977e2ef47b843b2ac10577efd351
Dharmik Patel <73016130+dharmikptl@users.noreply.github.com>
2022-03-13 19:10:32 +0530

    Add files via upload

---
+++ New folder/home.js
@@ -0,0 +1,23 @@
+function open_seed_Nav() {
+    document.getElementById("seed_nav").style.width = "100%";
+}
+
+function close_seed_Nav() {
+    document.getElementById("seed_nav").style.width = "0";
+}
+
+function open_pesticides_Nav() {
+    document.getElementById("pesticides_nav").style.width = "100%";
+}
+
+function close_pesticides_Nav() {
+    document.getElementById("pesticides_nav").style.width = "0";
+}
+
+function open_login_Nav() {
+    document.getElementById("login_nav").style.width = "100%";
+}
+
+function close_login_Nav() {
+    document.getElementById("login_nav").style.width = "0";
+}
